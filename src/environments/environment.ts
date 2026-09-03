ec3850f3e23cbce020f4a82d14c4d9aa1eb329fd
ChristianDuarteR <christianjavierduarte@gmail.com>
2026-09-02 22:27:14 -0500

    fix: endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  javaApi: 'https://micro-java-core.vercel.app',
-  pythonApi: 'https://micro-python.vercel.app',
+  javaApi: 'https://micro-java-core.onrender.com',
+  pythonApi: 'https://micro-python.onrender.com',
   metricsWsPath: '/ws/metrics',
-  metricsWsUrl: 'wss://micro-python.vercel.app/ws/metrics',
+  metricsWsUrl: 'wss://micro-python.onrender.com/ws/metrics',
 };
